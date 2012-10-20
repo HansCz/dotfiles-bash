b05b534c633612473ff6bee4dc89fe1347c7a72d
Hans Czajkowski Jørgensen <hans.c.jorgensen@gmail.com>
2012-10-20 14:05:41 +0200

    Made a few aliases conditional on being on my dev machine
      - Added m alias for macvim
      - Added Sublime Text alias 'subl'
    
    - changed the src alias from running ps1_set to
        loading $BASH/bashrc.bash

--- sections/aliases.bash
+++ sections/aliases.bash
@@ -1,3 +1,23 @@
+if [[ "$OSNAME" == "$DEFAULT_OSNAME" && "$HOSTNAME" == "$DEFAULT_HOSTNAME" ]] 
+then
+  # MacVim alias
+  alias m='mvim'
+  
+  # ST2 alias
+  alias subl='/Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl'
+
+  # Easier reloading of zshell environment for development
+  alias src="source $BASH/bashrc.bash"
+
+  # To prevent slowdown from humongous logs
+  alias nukelog='sudo rm -rf /private/var/log/asl/*.asl'
+
+  # Starting and stopping postgres as installed by homebrew
+  alias pgstart='pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start'
+  alias pgstop='pg_ctl -D /usr/local/var/postgres stop -s -m fast'
+fi
+
+
 # Git
 if [[ `command -v git` ]]; then
   alias gst='git status'
@@ -15,10 +35,3 @@ alias ...='cd $OLDPWD'
 # Miscellaneous shortcuts
 
 alias hg='history | grep '
-
-alias src='source ~/bin/util/ps1/ps1.bash;ps1_set'
-
-alias nukelog='sudo rm -rf /private/var/log/asl/*.asl'
-
-alias pgstart='pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start'
-alias pgstop='pg_ctl -D /usr/local/var/postgres stop -s -m fast'
